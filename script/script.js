b0339d718079da73e6ae75535a57c0eb1fb01ce7
Alexandre Silva <93850317+alexsilvabello@users.noreply.github.com>
2022-01-18 15:22:57 -0300

    Atualizando o Jscript
    Adicionando a função de controle de entrada e criptografia da palavra / frase

--- script/script.js
+++ script/script.js
@@ -1,4 +1,32 @@
 
+// Função que remove caracteres especiais / acentos para codificar/decodificar a frase
+function normalizar(frase){
+    fraselimpa = frase.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    return fraselimpa;
+
+}
+
+// Botão para leitura do campo input e codificação da frase.
+var botaoCriptografar = document.querySelector("#btn-cripto");
+botaoCriptografar.addEventListener("click", function() {
+    event.preventDefault();
+    var formcodificar = document.querySelector('#criptografa');
+    var formmensagem = document.querySelector('#mensagem')
+    var caixatexto = document.querySelector('#msg')
+    var frasecriptografar = formcodificar.inputtexto.value;
+    frasecriptografar = normalizar(frasecriptografar);
+    var frasecodificada = codificar(frasecriptografar);
+    caixatexto.value = frasecodificada;
+});
+
+// Função para codificar a palavra / frase
+function codificar(frase) {
+    frasecod = frase.replace(/i/gi,"imes").replace(/e/gi,"enter").replace(/a/gi, "ai").replace(/o/gi,"ober").replace(/u/gi,"ufat").toLowerCase();
+    return frasecod;
+}
+
+
+
 /* Regras Codificador: 
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
